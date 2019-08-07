879a821a0d40bb82ac12e057e434fe68a8b4b7e7
Jemima Abu <jemimaabu@gmail.com>
2019-08-07 09:57:32 +0100

    Included tabbable toggle

--- static/js/blog.js
+++ static/js/blog.js
@@ -1,7 +1,17 @@
 const root = document.documentElement;
-root.classList.add('dark');
+root.classList.add("dark");
+var themeToggle = document.getElementById("theme-toggle");
 
-document.getElementById('theme-toggle').addEventListener('click', function() {
-  root.classList.toggle('light');
-  root.classList.toggle('dark')
+themeToggle.addEventListener("click", function() {
+  root.classList.toggle("light");
+  root.classList.toggle("dark");
 });
+
+themeToggle.addEventListener("keypress", function(event) {
+    var keycode = event.keyCode ? event.keyCode : event.which;
+    if (keycode == 13) {
+      themeToggle.checked ? themeToggle.checked = false : themeToggle.checked = true
+      root.classList.toggle("light");
+      root.classList.toggle("dark");
+    }
+  });
